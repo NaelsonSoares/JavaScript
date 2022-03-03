e4ccbc3e5ad32462974755c29ac7758566188145
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2022-03-02 22:12:18 -0300

    ex variáveis

---
+++ exercícios js/variáveis/variaveis.js
@@ -0,0 +1,14 @@
+// var, let e const
+// var -> ES5 - usada em todos os browser
+// let e const -> ES2015 - pode ser usadas para também escrever variáveis e praticamente todos os browser já estão interpretando-os.
+
+let teste = 'minha string'
+console.log(teste)
+
+teste1 = 10
+console.log(teste1)
+
+const teste2 = 20
+console.log(teste2)
+
+// O let e o const não permite utilizar o mesmo nome de uma variável existente ao contrario do var
