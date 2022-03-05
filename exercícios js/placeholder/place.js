8d503c62a3c6194db444822951515b0c77b5f6be
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2022-03-04 22:14:37 -0300

    ex placeholder

---
+++ exercícios js/placeholder/place.js
@@ -0,0 +1,7 @@
+var nome = 'Melão'
+var idade = 132
+var sexo = 'masculino'
+var civil = 'casado'
+var esposa = 'Jáca'
+var filhos = 'Abacate e Manga'
+console.log(`Meu nome é ${nome} e tenho ${idade} anos do sexo ${sexo} sou ${civil} com ${esposa} e pai de ${filhos}.`)
