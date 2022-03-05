dc4a074abc30a19b82e25c0e989c1bdba817f647
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2022-03-05 17:28:56 -0300

    ex tipos primitivos

--- exercícios js/tipos primitivos/tipos.js
+++ exercícios js/tipos primitivos/tipos.js
@@ -1,4 +1,4 @@
-//String, number (int, float), booleam (true, false) são os tipos primitivos
+//String, number (int, float), booleam (true, false), null, undefined e Symbol são os tipos primitivos
 let minhaVar = 'minha string'
 let minhavar = 'minha "string" com aspas duplas'
 let var2 = "minha 'string' e com aspas simples"
@@ -15,5 +15,16 @@ console.log(typeof minhaVar, typeof msg, typeof idade, typeof msg_idade)// tipos
 const n1 = 40
 const n2 = 22.6
 console.log(`O tipo de n1 é ${typeof n1} e o seu valor é ${n1}`)
-console.log(`O tipo de n1 é ${typeof n2} e o seu valor é ${n2}`)
+console.log(`O tipo de n2 é ${typeof n2} e o seu valor é ${n2}`)
 
+var varteste = 
+console.log(varteste)
+console.log(typeof varteste)
+varteste = 42
+console.log(typeof varteste,  varteste)
+
+var varteste = null
+console.log(varteste)
+console.log(typeof varteste) /*informa um Object quando o valor da variável for o null. Sendo isto uma falha já antiga que está deste o inicio e não pedondo mais ser corrigido.*/ 
+varteste = 42
+console.log(typeof varteste, varteste)
