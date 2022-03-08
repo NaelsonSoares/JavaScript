9138e3315a5de633cc2afb3f21eddafb368bd65c
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2022-03-08 20:04:43 -0300

    Update opera-logcos.js

--- exercícios js/operadores/opera-logcos.js
+++ exercícios js/operadores/opera-logcos.js
@@ -1,4 +1,5 @@
 /* os lógicos são:
 ! (exclamação) negação
 && (e-comercial duplo) 'e' (conjução)
-|| (pipe duplo) 'ou' (disfunção) */
+|| (pipe duplo) 'ou' (disfunção) 
+a ordem de precedência é !, &&, ||*/
