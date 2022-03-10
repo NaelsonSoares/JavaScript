f58ba06d762535b2173efa6d481ef55a0a550ece
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2022-03-09 21:28:27 -0300

    ex opradores lógicos

---
+++ exercícios js/operadores/opera-condicionais.js
@@ -0,0 +1,28 @@
+var idade = 17
+var paisPresentes = true
+var bilheteComprado = true
+if(!bilheteComprado){
+    console.log("Não compou o bilhete")
+} else {
+    if(idade >= 18){
+        console.log("É maior de idade, pode viajar")
+    } else{
+        console.log("Opa!! é menor de idade")
+    }
+}
+console.log( )
+var n1 = 4
+var n2 = 7
+var n3 = 9
+var n4 = 6
+const media = (n1 + n2 + n3 + n4) / 4
+console.log(`media: ${media}`)
+
+if(n1 === 0 || n2 === 0){
+    console.log("Reprovado")
+} else if(media < 7){
+    console.log("Recuperação")
+} else{
+    console.log("Aprovado")
+}
+    console.log("Saiu do bloco if")
--- exercícios js/operadores/opera-logcos.js
+++ exercícios js/operadores/opera-logcos.js
@@ -3,3 +3,17 @@
 && (e-comercial duplo) 'e' (conjução)
 || (pipe duplo) 'ou' (disfunção) 
 a ordem de precedência é !, &&, ||*/
+/*
+Para uma pessoa viajar para o exterior:
+- Precisa ter 18 anos
+ou
+estar acompanhado dos pais
+e
+ter comprado o bilhete
+*/
+var idade = 17
+var paisPresentes = true
+var bilheteComprado = true
+const podeViajar = (idade >= 19 || paisPresentes) && bilheteComprado
+console.log(`pode viajar: ${podeViajar}`)
+console.log( )
