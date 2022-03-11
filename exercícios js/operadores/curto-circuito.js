817ef9391823dc308735cdca6270549e5d62b8cf
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2022-03-10 21:29:37 -0300

    operadores falsy-truthy e curto circuito

---
+++ exercícios js/operadores/curto-circuito.js
@@ -0,0 +1,12 @@
+
+var n = 1
+
+n = n || 10
+console.log(n)
+
+var isValid = true
+//if(isValid){
+  //  console.log('é válido')  isso pode ser substuido pelo codigo abaixo
+//}
+
+isValid && console.log('é válido')
---
+++ exercícios js/operadores/falsy-truthy.js
@@ -0,0 +1,9 @@
+// Falsy values: 0, "", NaN, undedfined, null, false
+// truthy values: todos os demais
+
+var n
+if( n ){
+    console.log("true")
+} else{
+    console.log("false")
+}
