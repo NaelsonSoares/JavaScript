a0f7565ad40d91a9d87c287af43b8daa8555dffc
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2022-03-12 07:37:13 -0300

    atualização loops

--- exercícios js/break e continue/script/script.js
+++ exercícios js/break e continue/script/script.js
@@ -1,13 +1,19 @@
+
 var numero = prompt('Digite um número')
 
-var indice = 11
+var indice = 0
 while(indice <= 10){
-    document.write(`${numero} x ${indice} = ${(indice * numero)}<br>`)
     indice++// é necessario fazer a variáel somar por ela mesmo para não entra no loop infinito.
+    document.write(`${numero} x ${indice} = ${(indice * numero)}<br>`)
 }
 document.write('saiu do loop while<br>')
 
-do{// serve para para o loop while quando a variável for maior que o elemento
+indice = 0
+do{
+    if(indice > 10){
+        alert('indice maior que 10')
+        break
+    }    
     document.write(`${numero} x ${indice} = ${(indice * numero)}<br>`)
     indice++
 }while(indice <= 10)
@@ -15,6 +21,9 @@ document.write('saiu do loop do while<br>')
 
 
 for(let tabuada = 0; tabuada <= 10; tabuada++){
+    if(tabuada === 5){
+        continue        
+    }
     document.write(`${numero} x ${tabuada} = ${(tabuada * numero)}<br>`)
 }
 document.write('saiu do loop for<br>')
