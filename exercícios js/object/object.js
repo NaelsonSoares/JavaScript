ff3bd9ff49089dd460c08d7f37d39c06760dc351
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2022-03-16 05:54:48 -0300

    ex object

---
+++ exercícios js/object/object.js
@@ -0,0 +1,20 @@
+
+    //um codigo fragil para erros
+//const nome = ['Daniel', 'Maria', 'João'] 
+//const idade = [50, 60, 40]
+//console.log(nome[0], idade[0]) 
+
+//sintaxe informal
+//const pessoa = new Object()
+//pessoa.nome = 'Daniel'
+//pessoa.idade = 40
+
+//sintaxe literal
+const pessoa = {
+    nome: 'Daniel',
+    idade: 40
+}
+    console.log(pessoa)
+    let propriedade = 'nome'
+    console.log(pessoa[propriedade])
+    console.log(pessoa.idade)
