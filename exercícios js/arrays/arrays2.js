837e5184b7f3de071d61f7f148f6c4d928a1cb8c
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2022-03-16 06:17:26 -0300

    ex arrays2 iterar

---
+++ exercícios js/arrays/arrays2.js
@@ -0,0 +1,5 @@
+
+const nomes = ['José', 'Renata', 'Josefa', 'Renato']
+    for (let indice = 0; indice < nomes.length; indice++){
+        console.log(`${indice}: o nome é ${nomes[indice]}`)
+    }
