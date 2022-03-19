ade9ef06615c7380a0b23485efc6b5eb71b8e969
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2022-03-19 16:37:11 -0300

    ex hoisting

--- exercícios js/funções/hoisting.js
+++ exercícios js/funções/hoisting.js
@@ -1,8 +1,18 @@
 
-
-teste()//chamandoa função antes s
+/*
+teste()//chamando a função antes da declaração da mesma, isto é um hoisting
 
 function teste(){
-    console.log('teste')
+   console.log('teste')
 }
 //teste()
+*/
+
+/*
+teste() antes da declaração function expression o hoisting não funciona
+
+const teste = function teste(){
+    console.log('teste')
+}
+teste()
+*/
