8f7cc3db33836be14116f5b2112852fb7938e556
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2022-06-14 13:06:09 -0300

    eventos data e hora javascript

--- exercícios js/eventos_diaHora/script.js
+++ exercícios js/eventos_diaHora/script.js
@@ -1,9 +1,9 @@
 function visualizar(){
     let msg = document.getElementById('msg')
     let img = document.getElementById('imagem')
-    //let foto = document.getElementById('foto')
+    let foto = document.getElementById('foto')
     let agora = new Date()
-    //let h = 13
+    //let h = 
     let h = agora.getHours()
     let m = agora.getMinutes()
     let hm = [h, m].join(':')
@@ -11,13 +11,19 @@ function visualizar(){
  if (h < 12){
     msg.innerHTML += ` Bom dia!`
     img.src= 'imagens/manha.png'
+    foto.src= 'imagens/fotomanha.jpg'
+    document.body.style.background = '#6FA7FF'
  }else if(h >= 12 && h < 18){
     msg.innerHTML += ` Boa Tarde!`
     img.src= 'imagens/tarde.png'
-    //foto.src = 'imagens/fototarde.jpg'
+    foto.src= 'imagens/fototarde.jpg'
+    document.body.style.background = '#FF943B'
  }else{
     msg.innerHTML += ' Boa noite!'
     img.src= 'imagens/noite.png'
+    foto.src= 'imagens/fotonoite.jpg'
+    document.body.style.color = '#ffffff'
+    document.body.style.background = '#762824' 
     
  }
 }
