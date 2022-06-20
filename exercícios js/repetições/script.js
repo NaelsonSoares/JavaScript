692500a3b11a097f473faf05efd253c483852e4c
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2022-06-20 18:54:42 -0300

    loop

---
+++ exercícios js/repetições/script.js
@@ -0,0 +1,21 @@
+
+/*let cont = 1                              
+while(cont <= 5){
+    console.log(`conta: ${cont}`)
+    cont++
+}
+do{                                                 
+    console.log(`loop do while: ${cont}`)
+    cont++
+}while(cont <= 5)*/
+
+//for(inicio; teste; incremento) inicio = let cont = 1; teste = cont <= 5; incremento = cont++
+/*for(let cont = 1; cont <= 5; cont++){
+    console.log(cont)
+}*/
+
+console.log('inicio da execução')
+for(let cont = 1; cont <= 5; cont++){
+    console.log(cont)
+}
+console.log('fim da execução')
